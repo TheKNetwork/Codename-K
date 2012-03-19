0d8e6af1c108c6700a7ed120ac0485dcc8b10f88
christophercoy <christophercoydev@gmail.com>
2012-03-18 19:50:08 -0500

    Adding a basic model entry for a learning session

--- codenamek/meeting_center/models.py
+++ codenamek/meeting_center/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class LearningSession(models.Model):
+    learningSessionId = models.CharField(max_length=250)
+    learningSessionTitle = models.CharField(max_length=250)
+    active = models.BooleanField()
+    description = models.TextField()
+    createdOn = models.TimeField()
+    slug = models.SlugField(unique=True)
+    
+    def __unicode__(self):
+        return self.learningSessionTitle
+    
+    
