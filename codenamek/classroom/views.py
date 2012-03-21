f33140123bae82792f2cdb331c4b05f5991212c5
christophercoy <christophercoydev@gmail.com>
2012-03-20 21:06:06 -0500

    Adding some basic user and group tests to the /classroom url

--- codenamek/classroom/views.py
+++ codenamek/classroom/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+# from django.shortcuts import redirect
+from django.shortcuts import render_to_response
+# from django.template import RequestContext
+from django.contrib.auth.models import Group, User
+
+import logging
+logger = logging.getLogger('dev')
+
+def index(request):
+    groupList = Group.objects.all()
+    logger.debug('Count of groups: ' + str(groupList.count()))
+    
+    userList  = User.objects.all()
+    logger.debug('Count of users: ' + str(userList.count()))
+    
+    return render_to_response("user_home.html", {'groups': groupList, 'users': userList} )
--- codenamek/settings.py
+++ codenamek/settings.py
@@ -160,16 +160,34 @@ EMAIL_FILE_PATH = '/srv/app-messages' # change this to a proper location
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'verbose'
         }
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
+            'propagate': True,
+        },
+        'dev': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
             'propagate': True,
         },
     }
