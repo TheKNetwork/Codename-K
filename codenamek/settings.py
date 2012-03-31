57ab124dd73a317b70a30cabf8c3102654f29c1b
Chris Coy <christophercoydev@gmail.com>
2012-03-31 16:15:33 -0500

    Removing dependencies that should be installed via pip

--- codenamek/settings.py
+++ codenamek/settings.py
@@ -4,6 +4,9 @@ from ConfigParser import RawConfigParser
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), os.path.pardir))
 
+STATIC_FILE_PATH = os.path.join(os.path.dirname(__file__), 'static')
+TEMPLATE_FILE_PATH = os.path.join(os.path.dirname(__file__), 'templates')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -92,14 +95,14 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/srv/www/codenamek/static',
+    STATIC_FILE_PATH,
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
@@ -109,8 +112,8 @@ SECRET_KEY = 'd$y-3-@&ljccptb1c)4fe9%qvy-6x%3!bw70t%b9yd=nqbik1*'
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.app_directories.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -124,7 +127,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'codenamek.urls'
 
 TEMPLATE_DIRS = (
-    "/srv/www/codenamek/templates",
+    TEMPLATE_FILE_PATH,
 )
 
 INSTALLED_APPS = (
