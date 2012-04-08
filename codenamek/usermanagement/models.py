1fedcce1a516db82a263bc1b724f2a894009d96f
Chris Coy <christophercoydev@gmail.com>
2012-04-07 20:19:14 -0500

    Rearranging model so that schools AND classes are groups, letting the extended objects be concerned with the relationships.
    Creating a school autocreates a group, and creating a class autocreates a group, without signals

--- codenamek/usermanagement/models.py
+++ codenamek/usermanagement/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext as _
-     
 from django.db.models import signals
+from django.core.exceptions import ObjectDoesNotExist
+
 from codenamek.usermanagement.signals import *
 
 # When model instance is saved, trigger creation of corresponding profile
 signals.post_save.connect(create_profile, sender=User)
 
-def add_user_to_class(class_id, user_id):
-    # Get the class from the class id
-    # query - 
-    
-    # Get the user object from the user id
-    # user.groups.add(school_class.group)
-    # user.save()
-    pass
+def add_school(**kwargs):
+    school = School.objects.create(**kwargs)
+    school.name = 'school.%s' % school.school_name
+    school.save()
+    return school
+
+def add_class(school_id, **kwargs):
+    existing_school = School.objects.get(id=school_id)
+    school_class = Class.objects.create(school=existing_school, **kwargs)
+    school_class.name = 'class.%s.%s' % (existing_school.school_name, school_class.class_name)
+    school_class.save()
+    return school_class
 
 SCHOOL_GENDER_FLAG_CHOICES = (
     ('B','Boys Only'),
@@ -28,20 +33,8 @@ class UserHistory(models.Model):
     event = models.CharField(max_length=255)
     event_time = models.DateTimeField()
 
-class Class(models.Model):
-    group = models.ForeignKey(Group, unique=True)
-    class_name = models.CharField(max_length=50)
-    class_description = models.TextField(blank=True)
-
-    class Meta:
-        verbose_name = _('Class')
-        verbose_name_plural = _('Classes')
-
-    def __unicode__(self):
-        return u"{0}".format(self.group)
-
-class School(models.Model):
-    school_name = models.CharField(max_length=100)
+class School(Group):
+    school_name = models.CharField(max_length=100, unique=True)
     address_line_one = models.CharField(max_length=100, blank=True)
     address_line_two = models.CharField(max_length=100, blank=True)
     address_city = models.CharField(max_length=100, blank=True)
@@ -49,8 +42,8 @@ class School(models.Model):
     address_country = models.CharField(max_length=50, blank=True)
     school_url = models.URLField(blank=True)
     gender_flag = models.CharField(max_length=1, choices=SCHOOL_GENDER_FLAG_CHOICES, blank=True)
-    classes = models.ManyToManyField(Class, blank=True)
-    users = models.ManyToManyField(User, blank=True)
+    
+    objects = models.Manager()
     
     class Meta:
         verbose_name = _('School')
@@ -58,6 +51,21 @@ class School(models.Model):
 
     def __unicode__(self):
         return self.school_name    
+    
+
+class Class(Group):
+    class_name = models.CharField(max_length=50)
+    class_description = models.TextField(blank=True)
+    school = models.ForeignKey(School)
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = _('Class')
+        verbose_name_plural = _('Classes')
+
+    def __unicode__(self):
+        return u"{0}".format(self.class_name)    
     
 class UserProfile(models.Model):
     personal_url = models.URLField(blank=True)
