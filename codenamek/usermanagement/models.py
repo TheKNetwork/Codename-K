56ca095353b29238e011c1a3c802b5023e993960
Chris Coy <christophercoydev@gmail.com>
2012-04-09 02:09:51 -0500

    Starting ticket 37 homeroom page changes

--- codenamek/usermanagement/models.py
+++ codenamek/usermanagement/models.py
@@ -25,9 +25,6 @@ def add_class(school_id, **kwargs):
 def get_schools_for_user(**kwargs):
     user = User.objects.get(**kwargs)
     schools = School.objects.filter(group_ptr=user.groups.filter(name__startswith="school."))
-    for school in schools:
-        school.class_set.all()
-        
     return schools
 
 SCHOOL_GENDER_FLAG_CHOICES = (
