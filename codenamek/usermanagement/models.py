7ea1cc9e78a56dd876fd222621b95c7c6910691b
Chris Coy <christophercoydev@gmail.com>
2012-04-10 18:00:29 -0500

    Separating the business logic of usermanagement and the model objects themselves into separate files

--- codenamek/usermanagement/models.py
+++ codenamek/usermanagement/models.py
@@ -1,3 +1,9 @@
+"""
+This file is responsible for the db set up and relationships of user management.
+The actual methods used to manipulate these objects should be placed in
+usermanagement_api.py.
+"""
+
 from django.db import models
 from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext as _
@@ -9,69 +15,6 @@ from codenamek.usermanagement.signals import *
 
 # When model instance is saved, trigger creation of corresponding profile
 signals.post_save.connect(create_profile, sender=User)
-
-def add_school(**kwargs):
-    school = School.objects.create(**kwargs)
-    school.name = 'school.%s' % school.school_name
-    school.save()
-    return school
-
-def add_class(school_id, **kwargs):
-    existing_school = School.objects.get(id=school_id)
-    school_class = Class.objects.create(school=existing_school, **kwargs)
-    school_class.name = 'class.%s.%s' % (existing_school.school_name, school_class.class_name)
-    school_class.save()
-    return school_class
-
-def get_schools_for_user(**kwargs):
-    user = User.objects.get(**kwargs)
-    schools = School.objects.filter(group_ptr=user.groups.filter(name__startswith="school."))
-    return schools
-
-def get_main_school_for_user(**kwargs):
-    user = User.objects.get(**kwargs)
-    user_profile = UserProfile.objects.get(user__id=user.id)
-    return user_profile.main_school
-
-def invite_user_to_class(inviting_user_id, invited_user_id, school_class_id):
-    invited_by = User.objects.get(id=inviting_user_id)
-    invited_user = User.objects.get(id=invited_user_id)
-    school_class = Class.objects.get(id=school_class_id)
-    class_invitation = ClassInvitation.objects.create(
-                            school_class=school_class,
-                            invited_user=invited_user,
-                            invited_by=invited_by
-                        )
-    return class_invitation
-    
-def invite_user_to_class_by_lookup_names(inviting_user_name, invited_user_name, school_name, school_class_name):
-    print "inviting user name %s" % inviting_user_name
-    print "invited user name %s" % invited_user_name
-    ccoy = User.objects.get(username=inviting_user_name)
-    mustefa = User.objects.get(username=invited_user_name)
-    school = School.objects.get(school_name=school_name)
-    school_class = school.class_set.filter(class_name=school_class_name)[0]
-    return invite_user_to_class(ccoy.id, mustefa.id, school_class.id)
-    
-def accept_invitation_to_class(current_user, invitation_id):
-    existing_invitations = current_user.invitations_received.filter(id=invitation_id)
-    for invitation in existing_invitations:
-        if invitation.id == invitation_id:
-            invitation.accepted_on_date = datetime.datetime.now()
-            invitation.save()
-            return invitation
-        
-    raise "No matching invitation found."
-
-def reject_invitation_to_class(current_user, invitation_id):
-    existing_invitations = current_user.invitations_received.filter(id=invitation_id)
-    for invitation in existing_invitations:
-        if invitation.id == invitation_id:
-            invitation.rejected_on_date = datetime.datetime.now()
-            invitation.save()
-            return invitation
-        
-    raise "No matching invitation found."
 
 SCHOOL_GENDER_FLAG_CHOICES = (
     ('B','Boys Only'),
