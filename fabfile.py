d1c6a1f683f737d05b58dfaa19c9749d60bb0f54
Chris Coy <christophercoydev@gmail.com>
2012-04-11 14:32:27 -0500

    Fixing fab for env changes on rack

--- fabfile.py
+++ fabfile.py
@@ -30,22 +30,21 @@ def checkout_latest():
     """Pull the latest code into the git repo and copy to a timestamped release directory"""
     import time
     env['release'] = time.strftime('%Y%m%d%H%M%S')
-    run('cd %(path)s/%(repository)s; git pull origin master' % env)
-    run('cp -R %(path)s/%(repository)s %(path)s/releases/%(release)s; rm -rf %(path)s/releases/%(release)s/.git*' % env)
+    run('cd %(path)s/%(repository)s; git pull origin master' % env, pty=True)
+    run('cp -R %(path)s/%(repository)s %(path)s/releases/%(release)s; rm -rf %(path)s/releases/%(release)s/.git*' % env, pty=True)
 
 def symlink_current_release():
     """Symlink our current release"""
-    run('cd %(path)s/releases; rm current; ln -s %(release)s current' % env)
+    run('cd %(path)s/releases; rm current; ln -s %(release)s current' % env, pty=True)
 
 def install_requirements():
     """Install the required packages using pip"""
-    run('cd %(path)s/releases/current; %(path)s/python/bin/pip install -r %(path)s/releases/current/dependencies.txt' % env)
+    run('cd %(path)s/releases/current; %(path)s/python/bin/pip install -r %(path)s/releases/current/dependencies.txt' % env, pty=True)
 
 def migrate():
     """Run our migrations"""
-    run('export DJANGO_ENV=%(release_type)s; cd %(path)s/releases/current/codenamek;  %(path)s/python/bin/python manage.py syncdb --noinput' % env)
+    run('export DJANGO_ENV=%(release_type)s; cd %(path)s/releases/current/codenamek;  %(path)s/python/bin/python manage.py syncdb --noinput' % env, pty=True)
 
 def restart_apache():
     "Reboot Apache2 server."
-    sudo("service apache2 reload")
-
+    sudo("service apache2 reload",pty=True)
