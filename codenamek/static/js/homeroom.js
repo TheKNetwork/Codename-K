2da1c0e19ea006af169f0c7e4df7066625fdfaa4
mustefa jo'shen <mail@mustefa.com>
2012-04-15 12:02:53 -0400

    Hiding of create class row working + styling fixes

--- codenamek/static/js/homeroom.js
+++ codenamek/static/js/homeroom.js
@@ -2,13 +2,15 @@
 $("a[rel=users]").popover();
 
 //Shows Create Class input row if .create-class is clicked 
-$(".create-class").bind('click', function () {
-	$("#create-class-row").css("display", "table-row");
+$("#create-class").bind('click', function () {
+	$("#create-class-row").show("slow");
+//	$("#create-class-row").css("display", "table-row");
 	$("#id_class_name").focus();
 });
 
 //Hides Create Class input row if .cancel-class-creation is clicked
-$(".cancel-class-creation'").bind('click', function () {
-	$("#create-class-row").css("display", "none");
+$("#cancel-class-creation").bind('click', function () {
+	$("#create-class-row").hide()
+//	$("#create-class-row").css("display", "hidden");
 });
 
