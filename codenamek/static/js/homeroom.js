faf8a23b6d2d8cfc63f2fcf4bab059103e7c7736
mustefa jo'shen <mail@mustefa.com>
2012-04-15 15:10:50 -0400

    Few more style updates.

--- codenamek/static/js/homeroom.js
+++ codenamek/static/js/homeroom.js
@@ -4,13 +4,12 @@ $("a[rel=users]").popover();
 //Shows Create Class input row if .create-class is clicked 
 $("#create-class").bind('click', function () {
 	$("#create-class-row").show("slow");
-//	$("#create-class-row").css("display", "table-row");
 	$("#id_class_name").focus();
+	$("#create-class").addClass("disabled");
 });
 
 //Hides Create Class input row if .cancel-class-creation is clicked
 $("#cancel-class-creation").bind('click', function () {
 	$("#create-class-row").hide()
-//	$("#create-class-row").css("display", "hidden");
+	$("#create-class").removeClass("disabled");
 });
-
