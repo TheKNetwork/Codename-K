10bd6eaf511af63d320415b25eb51e48ddd84e7d
mustefa jo'shen <mail@mustefa.com>
2012-04-15 04:01:02 -0400

    Added logic to hide and show classroom row when create / cancel class actions are triggered

--- codenamek/static/js/homeroom.js
+++ codenamek/static/js/homeroom.js
@@ -1,1 +1,11 @@
 $("a[rel=users]").popover();
+
+$(".create-class").bind('click', function () {
+	$("#create-class-row").css("display", "table-row");
+	$("#id_class_name").focus();
+});
+
+$(".cancel-class-creation'").bind('click', function () {
+	$("#create-class-row").css("display", "none");
+});
+
