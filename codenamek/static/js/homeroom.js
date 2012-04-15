f0a00fd16af6916271e292d86e8b51bf1ac11f3d
mustefa jo'shen <mail@mustefa.com>
2012-04-15 04:12:56 -0400

    Added comments and additional formatting.  Brought create class input to the top row for easy visibility.

--- codenamek/static/js/homeroom.js
+++ codenamek/static/js/homeroom.js
@@ -1,10 +1,13 @@
+//popover on # of users to show the users in a class
 $("a[rel=users]").popover();
 
+//Shows Create Class input row if .create-class is clicked 
 $(".create-class").bind('click', function () {
 	$("#create-class-row").css("display", "table-row");
 	$("#id_class_name").focus();
 });
 
+//Hides Create Class input row if .cancel-class-creation is clicked
 $(".cancel-class-creation'").bind('click', function () {
 	$("#create-class-row").css("display", "none");
 });
