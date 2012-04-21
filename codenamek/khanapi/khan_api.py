c9816e836129af1b32e97fe98e63ba9e82597a15
Chris Coy <christophercoydev@gmail.com>
2012-04-21 14:13:47 -0500

    Made the khan api stuff, including exercises and proficiency, more flexible, callable from a non web context, and testable. Wrote tests to proof concepts to use in larger test

--- codenamek/khanapi/khan_api.py
+++ codenamek/khanapi/khan_api.py
@@ -29,68 +29,74 @@ def is_khan_user_active(request):
     active_khan_user = False
     if request.user.get_profile() is not None:
         if request.user.get_profile().access_token is not None:
-            print "Found Khan API access token for user %s" % request.user
             request.session['oauth_token_string'] = request.user.get_profile().access_token
             active_khan_user = True
     return active_khan_user;
 
-def get_khan_user(request):
-    if not is_khan_user_active(request):
-        return ''
-    return get_json_for_khan_api_call(request, '/api/v1/user')   
+def get_khan_user(user):
+    return execute_khan_api_method(user.get_profile().access_token, '/api/v1/user')   
 
-def get_khan_exercises(request):
-    if not is_khan_user_active(request):
-        return ''
-    return get_json_for_khan_api_call(request, '/api/v1/exercises')  
+def get_khan_exercises(user):
+    return execute_khan_api_method(user.get_profile().access_token, '/api/v1/exercises')  
 
-def get_khan_badges(request):
-    if not is_khan_user_active(request):
-        return ''
-    return get_json_for_khan_api_call(request, '/api/v1/badges') 
+def get_khan_badges(user):
+    return execute_khan_api_method(user.get_profile().access_token, '/api/v1/badges') 
 
-def get_khan_exercise_history(request):
-    if not is_khan_user_active(request):
-        return ''
-    return get_json_for_khan_api_call(request, '/api/v1/exercise_history') 
+def get_khan_exercise_history(user):
+    return execute_khan_api_method(user.get_profile().access_token, '/api/v1/exercise_history')
 
-def get_data_for_khan_api_call(request, url):
+# Returns a dict of exercise_states{ }
+def get_proficiency_for_exercise(user, exercise_name):
+    jsondata = execute_khan_api_method(user.get_profile().access_token, '/api/v1/user/exercises/%s' % exercise_name)  
+    print jsondata 
+    return jsondata['exercise_states']
+
+def get_exercise_for_user(user, exercise_name):
+    return execute_khan_api_method(user.get_profile().access_token, '/api/v1/user/exercises/%s' % exercise_name)   
+
+# This method is the funnel point for all khan api calls. It caches data based on
+# the user's access token and method passed in.
+# The default cache timeout is one hour, 60 seconds * 60 minutes
+# Regardless of the cache, a refresh can be forced by passing in force_refresh=True
+def execute_khan_api_method(profile_access_token, api_method, cache_timeout=3600, force_refresh=False, return_raw_text=False):
+    cache_key = "%s:%s:%s" % (profile_access_token, api_method, return_raw_text)
+    cache_hit = False
+    # print "Cache key for KHAN API Call is %s" % cache_key
+    result_data = cache.get(cache_key)
     
-    if not url:
-        abort(400)
-
-    # Returns a dictionary with keys: 'headers', 'body', and 'status'.
-    resource = CLIENT.access_api_resource(
-        url,
-        access_token(request.session),
-        method = request.method
-        )
-    
-    text = resource['body']
-
-    # Error messages can contain HTML. Escape them so they're not rendered.
-    is_html = has_text_html_header(resource['headers'])
-    if is_html:
-        text = cgi.escape(text)
-    
-    return text
-
-def get_json_for_khan_api_call(request, url):
-    cache_key = "%s:%s" % (request.user.username, url)
-    print "Cache key for KHAN API Call is %s" % cache_key
-    jsondata = cache.get(cache_key)
-    if jsondata is None or jsondata == '':
-        text = get_data_for_khan_api_call(request, url)
+    if result_data is None or result_data == '' or force_refresh:
+        resource = CLIENT.access_api_resource(
+            api_method,
+            access_token = OAuthToken.from_string(profile_access_token),
+            method = "GET"
+            )
+        
+        text = resource['body']
+        
+        # Error messages can contain HTML. Escape them so they're not rendered.
+        is_html = has_text_html_header(resource['headers'])
+        if is_html:
+            text = cgi.escape(text)
+        
         try:
-            jsondata = simplejson.loads(text)
+            if return_raw_text:
+                result_data = text
+            else:
+                print "converting to json"
+                result_data = simplejson.loads(text)
         except:
-            jsondata = ''
-        cache.set(cache_key, jsondata, 30)
+            result_data = ''
+        cache.set(cache_key, result_data, cache_timeout)
         
     else:
-        print "Got json data from cache!"
-         
-    return jsondata
+        #print "Got json data from cache!"
+        cache_hit = True
+    
+    if not cache_hit:
+        # update local tables with fresh data
+        print "(cache not hit or is empty: Update local tables with data)"
+    
+    return result_data
 
 def has_request_token(session):
     return 'request_token' in session
