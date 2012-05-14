dd5135da0c6469b5127852a2fccbc8a653cc1184
Chris Coy <christophercoydev@gmail.com>
2012-05-13 20:30:36 -0500

    Fixing missing import

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import get_object_or_404, render, redirect
 from django_socketio import broadcast, broadcast_channel, NoSocket
 from django.views.decorators.cache import *
-from chat.models import ChatRoom
 from schools.models import *
 
 @never_cache
